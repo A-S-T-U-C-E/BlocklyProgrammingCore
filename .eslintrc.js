0bf6e68b6fbc32b7b3a64058774f106b780a0a62
Preet P. Vadaliya <preetvadaliya.ict18@gmail.com>
2022-08-23 14:36:39 +0530

    Node project set-up with EsLint and Webpack.

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+/**
+ * @fileoverview EsLint configuration file.
+ * @author preetvadaliya@gmail.com (Preet P. Vadaliya)
+ */
+
+module.exports = {
+  'env': {
+    'browser': true,
+    'es2021': true,
+  },
+  'extends': 'google',
+  'overrides': [
+  ],
+  'parserOptions': {
+    'ecmaVersion': 'latest',
+    'sourceType': 'module',
+  },
+  'rules': {
+  },
+};
